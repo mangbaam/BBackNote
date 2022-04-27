9fa97c3fc4a3308151c41273ba6739e5dacfdaf4
mangbaam <pmb0836@gmail.com>
2022-04-28 02:50:25 +0900

    [feat] init setting - jetpack navigation, room, dataBinding

---
+++ app/src/main/java/mangbaam/bbacknote/MainActivity.kt
@@ -0,0 +1,11 @@
+package mangbaam.bbacknote
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
