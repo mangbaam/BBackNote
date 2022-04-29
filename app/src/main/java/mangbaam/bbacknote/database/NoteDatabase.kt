fc22125c6f83829634847bcf6c998f52aea21e53
mangbaam <pmb0836@gmail.com>
2022-04-30 05:12:49 +0900

    [refactor] migration DB ver1 to 2 (because of addition of Note title)

--- app/src/main/java/mangbaam/bbacknote/database/NoteDatabase.kt
+++ app/src/main/java/mangbaam/bbacknote/database/NoteDatabase.kt
@@ -4,10 +4,18 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import mangbaam.bbacknote.model.NoteEntity
 
-@Database(entities = [NoteEntity::class], version = 1)
-abstract class NoteDatabase: RoomDatabase() {
+val MIGRATION_1_2 = object: Migration(1, 2) {
+    override fun migrate(database: SupportSQLiteDatabase) {
+        database.execSQL("ALTER TABLE NoteEntity ADD COLUMN title TEXT NOT NULL DEFAULT ''")
+    }
+}
+
+@Database(entities = [NoteEntity::class], version = 2)
+abstract class NoteDatabase : RoomDatabase() {
     abstract fun noteDao(): NoteDao
 
     companion object {
@@ -20,7 +28,7 @@ abstract class NoteDatabase: RoomDatabase() {
                     instance = Room.databaseBuilder(
                         context.applicationContext, NoteDatabase::class.java,
                         "note-database"
-                    ).build()
+                    ).addMigrations(MIGRATION_1_2).build()
                 }
             }
             return instance
